5f9850491260d21b472463ec5bd1a8b4a0e3882f
Erterion <79190617+Erterion@users.noreply.github.com>
2021-04-25 21:04:52 +0300

    Акимкин Алексей

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,87 @@
 #include <string>
 #include "tstack.h"
 
+int priority(char c) {
+    switch (c) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+','-':
+        return 2;
+    case '*','/':
+        return 3;
+    default:
+        return -1;
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
+  TStack<char> stack1;
+    std::string stroka;
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            stroka += inf[i];
+            stroka += ' ';
+        }
+        else if ((inf[i] == '(') || (priority(inf[i]) > priority(stack1.get())) || (stack1.isEmpty())) {
+            stack1.push(inf[i]);
+        }
+        else if (inf[i] == ')') {
+            while (!stack1.isEmpty() && stack1.get() != '(') {
+                stroka += stack1.get();
+                stroka += ' ';
+                stack1.pop();
+            }
+            if (stack1.get() == '(')
+                stack1.pop();
+        }
+        else {
+            while ((!stack1.isEmpty()) && (priority(stack1.get()) >= priority(inf[i]))) {
+                stroka = stack1.get();
+                stroka += ' ';
+                stack1.pop();
+            }
+            stack1.push(inf[i]);
+        }
+    }
+            while (!stack1.isEmpty()) {
+                stroka += stack1.get();
+                stroka += ' ';
+                stack1.pop();
+            }
+    return stroka;
+}
   return std::string("");
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+  TStack<int> stack2;
+    int output;
+    for (int i = 0; i < pst.length(); i++) {
+        if ((pst[i] >= '0') && (pst[i] <= '9')) {
+            stack2.push(pst[i]- '0');
+        }
+        else
+            if (pst[i] != ' ') {
+                int a2 = stack2.get();
+                stack2.pop();
+                int a1 = stack2.get();
+                stack2.pop();
+                if (pst[i] == '-') {
+                    stack2.push(a1 - a2);
+                }
+                else if (pst[i] == '+') {
+                    stack2.push(a1 + a2);
+                }
+                else if (pst[i] == '*') {
+                    stack2.push(a1 * a2);
+                }
+                else {
+                    stack2.push(a1 / a2);
+                }
+            }
+    }
+    output = stack2.get();
+    return output;
 }
